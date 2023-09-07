91d44aff7472c83e858ae3b6f64b09d567bc19a7
red <karatasmert255@gmail>
2023-09-07 11:52:18 +0300

    slider completed (without library)

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ btnCloseSearch.addEventListener("click", function () {
   modalSearch[0].style.opacity = "0";
 });
 
-/* click outside start*/
+//?click outside start
 
 document.addEventListener("click", function (e) {
   if (
@@ -52,6 +52,47 @@ document.addEventListener("click", function (e) {
   }
 });
 
-/* click outside end */
+//?click outside end
 
 //!Search modal end
+
+//! silider start
+let slideIndex = 1;
+showSlides(slideIndex);
+
+setInterval(() => {
+  showSlides((slideIndex += 1));
+}, 4000);
+
+function plusSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slider-item");
+  const dots = document.getElementsByClassName("slider-dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
+
+//! silider end
