b6b585ae13a4a18c96ca74ad671678f2d1f11c8f
red <karatasmert255@gmail>
2023-09-07 13:24:09 +0300

    carousel completed with library (glide.js)

---
+++ js/glide.js
@@ -0,0 +1,37 @@
+const config = {
+  type: "carousel",
+  perView: 4,
+  gap: 20,
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
+
+new Glide(".product-carousel", config).mount();
+
+const config2 = {
+  type: "carousel",
+  perView: 4,
+  gap: 20,
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
+
+new Glide(".product-carousel2", config2).mount();
